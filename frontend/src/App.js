2ec5a4d30b71e43473629e754f5b42d0131296fe
misanr88 <misanr88@gmail.com>
2023-07-20 17:23:48 +0530

    1st compeletion page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,17 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage, SignupPage, ActivationPage } from "./Routes.js";
-import axios from "axios";
-import { server } from "./server";
+
+import Store from "./redux/store";
+import { loadUser } from "./redux/actions/user";
 
 const App = () => {
   useEffect(() => {
-    axios
-      .get(`${server}/user/getuser`, { withCredentials: true })
-      .then((res) => {
-        toast.success(res.data.message);
-      })
-      .catch((err) => {
-        toast.error(err.response.data.message);
-      });
+    Store.dispatch(loadUser());
   }, []);
 
   return (
