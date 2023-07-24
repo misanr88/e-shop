15eebe21225a3358b4e1d3acb21940b8c19c4c2f
misanr88 <misanr88@gmail.com>
2023-07-24 14:51:46 +0530

    fontend compelete

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-import Header from "../Components/Layout/Header.jsx";
+import Header from "../Components/Layout/Header";
+import Hero from "../Components/Route/Hero/Hero";
+import Categories from "../Components/Route/Categories/Categories";
+import BestDeals from "../Components/Route/BestDeals/BestDeals";
+import FeaturedProduct from "../Components/Route/FeaturedProduct/FeaturedProduct";
+import Events from "../Components/Events/Events";
+import Sponsored from "../Components/Route/Sponsored";
+import Footer from "../Components/Route/Footer";
 
 const HomePage = () => {
   return (
     <div>
-      <Header />
+      <Header activeHeading={1} />
+      <Hero />
+      <Categories />
+      <BestDeals />
+      <Events />
+      <FeaturedProduct />
+      <Sponsored />
+      <Footer />
     </div>
   );
 };
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,5 +2,17 @@ import LoginPage from "../src/Pages/Login";
 import SignupPage from "../src/Pages/SignupPage";
 import ActivationPage from "../src/Pages/ActivationPage";
 import HomePage from "../src/Pages/HomePage.jsx";
-
-export { LoginPage, SignupPage, ActivationPage, HomePage };
+import ProductsPage from "../src/Pages/ProductsPage";
+import BestSellingPage from "../src/Pages/BestSellingPage";
+import EventsPage from "../src/Pages/EventsPage.jsx";
+import FAQPage from "../src/Pages/FAQPage";
+export {
+  LoginPage,
+  SignupPage,
+  ActivationPage,
+  HomePage,
+  ProductsPage,
+  BestSellingPage,
+  EventsPage,
+  FAQPage,
+};
